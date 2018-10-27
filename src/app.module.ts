77d4ef1b0b99bd95fea29545d18463a4457bf8a5
Mika Hassinen <hassinen.mika@gmail.com>
2018-10-28 01:23:43 +0300

    chore: add new modules to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from 'user/user.module';
+import { ConfigModule } from './services/config/config.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(),
+    UserModule,
+    ConfigModule,
+    AuthModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
