f177da4252c93b93801bc2dd439dfc4b392c7cb4
Mika Hassinen <hassinen.mika@gmail.com>
2018-10-28 02:05:25 +0300

    chore(user): update swagger documentation

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,12 @@
-import { Controller, Get, Post, Body, Param, UseGuards, Headers, Res, HttpStatus } from '@nestjs/common';
+import { Controller, Get, Post, Body, UseGuards, Headers, Res, HttpStatus } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './create-user.dto';
 import { AuthGuard } from '@nestjs/passport';
 import { Response } from 'express';
+import { UserCredentialsDto } from './user-credentials.dto';
+import { ApiResponse, ApiBearerAuth, ApiUseTags } from '@nestjs/swagger';
 
+@ApiUseTags('user')
 @Controller('user')
 export class UserController {
     
@@ -11,17 +14,19 @@ export class UserController {
 
     @Get('index')
     @UseGuards(AuthGuard('bearer'))
+    @ApiBearerAuth()
     index() {
         return this.userService.index();
     }
 
-    @Post()
+    @Post('register')
     create(@Body() user: CreateUserDto) {
         return this.userService.create(user);
     }
 
     @Get('me')
     @UseGuards(AuthGuard('bearer'))
+    @ApiBearerAuth()
     me(@Headers() headers) {
         const authHeader: string = headers.authorization;
         const token = authHeader.substring(7, authHeader.length);
@@ -29,7 +34,9 @@ export class UserController {
     }
 
     @Post('login')
-    async login(@Body() credentials: CreateUserDto, @Res() response: Response) {
+    @ApiResponse({ status: 200, description: 'Login successful.'})
+    @ApiResponse({ status: 401, description: 'Login failed.'})
+    async login(@Body() credentials: UserCredentialsDto, @Res() response: Response) {
         try {
             const tokenResponse = await this.userService.login(credentials);
             response.status(HttpStatus.OK).json(tokenResponse);
