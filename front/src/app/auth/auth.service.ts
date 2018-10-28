316cc6d5c780195a54ae2c7f2e4e768a87276c07
Mika Hassinen <hassinen.mika@gmail.com>
2018-10-28 17:56:43 +0200

    chore(front): move login component functionality to auth service

--- front/src/app/auth/auth.service.ts
+++ front/src/app/auth/auth.service.ts
@@ -1,19 +1,45 @@
 import { Injectable } from '@angular/core';
+import { UserService } from '../api/services';
+import { UserCredentialsDto, TokenResponseDto, User } from '../api/models';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { StrictHttpResponse } from '../api/strict-http-response';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(
+    private readonly userService: UserService,
+    private readonly router: Router
+  ) { }
 
   setToken(token: string): void {
-    console.log('set token', token);
     localStorage.setItem('token', token);
   }
 
   getToken() {
-    console.log('get token', localStorage.getItem('token'));
     return localStorage.getItem('token') || null;
   }
+
+  login(credentials: UserCredentialsDto) {
+    this.userService.postApiUserLogin({
+      email: credentials.email, password: credentials.password
+    }).subscribe((response: TokenResponseDto) => {
+      this.setToken(response.token);
+      this.router.navigate(['/dashboard']);
+    }, (error: Error) => {
+      // TODO: show snack
+    });
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+  }
+
+  getCurrentUser(): Observable<User> {
+    return this.userService.getApiUserMe();
+  }
+
 }
--- front/src/app/login/login/login.component.ts
+++ front/src/app/login/login/login.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { UserService } from 'src/app/api/services';
 import { AuthService } from 'src/app/auth/auth.service';
-import { TokenResponseDto } from 'src/app/api/models';
+import { TokenResponseDto } from 'src/app/api/models/token-response-dto';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'saa-login',
@@ -15,7 +16,8 @@ export class LoginComponent implements OnInit {
 
   constructor(
     private readonly userService: UserService,
-    private readonly authService: AuthService
+    private readonly authService: AuthService,
+    private readonly router: Router
   ) {}
   ngOnInit() {
     this.loginForm = new FormGroup({
@@ -24,19 +26,8 @@ export class LoginComponent implements OnInit {
     });
   }
 
-  async onSubmit() {
-    console.log('this.loginForm.value', this.loginForm.value);
-    const values = this.loginForm.value;
-    console.log('email, password', values.email, values.password);
-    this.userService.postApiUserLogin({
-      email: values.email, password: values.password
-    }).subscribe((response: TokenResponseDto) => {
-      this.authService.setToken(response.token);
-      // TODO: redirect to dashboard
-    }, (error: Error) => {
-      // TODO: show snack
-    });
-    
+  onSubmit() {
+    this.authService.login(this.loginForm.value);
   }
 
 }
