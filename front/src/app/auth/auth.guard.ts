7034a60f2ac528bae001241030a20449fb5bfab7
Mika Hassinen <hassinen.mika@gmail.com>
2018-10-29 22:36:32 +0200

    chore: add auth guard

---
+++ front/src/app/auth/auth.guard.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  
+  constructor(public auth: AuthService, public router: Router) {}
+
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    if (this.auth.isLoggedIn()) return true;
+    this.router.navigate(['/login']);
+    return false;    
+  }
+
+}
