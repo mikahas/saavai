0412a696a00b075d80588b43089d44ecd977df07
Mika Hassinen <hassinen.mika@gmail.com>
2018-11-10 22:00:15 +0200

    chore: add ping pong

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Get, Controller } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiResponse } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,11 @@ export class AppController {
   @Get()
   root(): string {
     return this.appService.root();
+  }
+
+  @Get('ping')
+  @ApiResponse({ status: 200, description: 'Ping - pong, check API heartbeat' })
+  ping(): string {
+    return this.appService.ping();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,14 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+
   root(): string {
     return 'Sää Vai API';
   }
+
+  ping(): string {
+    // TODO: add database connection check
+    return 'pong';
+  }
+
 }
