87f8832d5c0da3ff170f01ab0a2931d1c1d81b5e
Mika Hassinen <hassinen.mika@gmail.com>
2018-11-10 21:58:46 +0200

    chore: fetch weather index with parameters determining the range and from date

---
+++ src/weather/query-weather-index.dto.ts
@@ -0,0 +1,18 @@
+import { ApiModelProperty } from '@nestjs/swagger';
+
+export class QueryWeatherIndexDto {
+
+    @ApiModelProperty({
+        required: false,
+        enum: ['year', 'month', 'week', 'day', 'hour'],
+        description: 'Time period range'
+    })
+    readonly range: string;
+
+    @ApiModelProperty({
+        required: false,
+        description: 'Starting date where time period will be calculated. Default is now.'
+    })
+    readonly from: Date;
+
+}
--- src/weather/weather.service.ts
+++ src/weather/weather.service.ts
@@ -4,6 +4,7 @@ import { Weather } from './weather.entity';
 import { Repository } from 'typeorm';
 import { CreateDropDto } from './create-drop.dto';
 import { User } from 'user/user.entity';
+import * as moment from 'moment';
 
 @Injectable()
 export class WeatherService {
@@ -13,11 +14,31 @@ export class WeatherService {
         private readonly weatherRepository: Repository<Weather>,
     ) {}
 
-    async index(user: User): Promise<Weather[]> {
-        return this.weatherRepository.find({
-            where: { user },
-            select: ['data', 'location']
-        });
+    async index(user: User, range: string, fromDate: string | Date = new Date()): Promise<Weather[]> {
+        let start = moment(fromDate);
+        let end = moment(fromDate);
+
+        if (!start.isValid()) throw new Error('Invalid from date');
+
+        switch (range) {
+            // TODO: filter results for year, month and week, calculate min/max for each month/day
+            // case 'year': start = start.subtract(1, 'years'); break;
+            // case 'month': start = start.subtract(1, 'months'); break;
+            case 'week': start = start.subtract(7, 'days'); break;
+            case 'hour': start = start.subtract(1, 'hours'); break;
+            case 'day':
+            default: start = start.subtract(24, 'hours');
+        }
+
+        return this.weatherRepository.createQueryBuilder('weather')
+            .where({ user })
+            .andWhere('weather.createdAt > :start', {
+                start: start.format(moment.HTML5_FMT.DATETIME_LOCAL_MS).toString()
+            })
+            .andWhere('weather.createdAt <= :end', {
+                end: end.format(moment.HTML5_FMT.DATETIME_LOCAL_MS).toString()
+            })
+            .getMany();
     }
 
     async create(drop: CreateDropDto, user: User): Promise<Weather> {
