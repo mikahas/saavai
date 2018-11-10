51166d508629fb963099b573a640565993e662e7
Mika Hassinen <hassinen.mika@gmail.com>
2018-11-10 22:05:44 +0200

    feat: add a button for api key regeneration, show existing api key

--- front/src/app/user/user/user.component.ts
+++ front/src/app/user/user/user.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { User } from 'src/app/api/models';
+import { UserService } from 'src/app/api/services';
 
 @Component({
   selector: 'saa-user',
@@ -10,13 +11,27 @@ import { User } from 'src/app/api/models';
 export class UserComponent implements OnInit {
 
   user: User;
+  isLoading: boolean = false;
 
-  constructor(private readonly route: ActivatedRoute) { }
+  constructor(
+    private readonly route: ActivatedRoute,
+    private readonly userService: UserService
+  ) { }
 
   ngOnInit() {
     this.route.data.subscribe((data: { user: User }) => {
       this.user = data.user;
     });
+  }
+
+  generateApiKey() {
+    this.isLoading = true;
+    this.userService
+      .postApiUserGenerateApiKey()
+      .subscribe((apiKey: string) => {
+        this.user.apiKey = apiKey;
+        this.isLoading = false;
+      });
   }
 
 }
